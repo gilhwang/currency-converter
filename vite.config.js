42522e47d46edfe47123568d527c97cb52fd329c
Gil Hwang <giyuhwang@gmail.com>
2024-04-21 10:58:32 -0400

    Added configuration files for deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const BASE_URL = 'currency-converter';
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: `/${BASE_URL}/`
+  base: `/currency-converter/`
 })
